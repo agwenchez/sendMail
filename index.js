c7c95babdeec9c4c8e87389c1905d1fabcbe4075
Agwenchez <agweraenock76@gmail.com>
2019-07-31 10:16:14 +0300

    send mail with nodemailer

---
+++ index.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+const log = console.log;
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL || 'agweraenock76@gmail.com', 
+        pass: process.env.PASSWORD || 'agwenchez'
+    }
+});
+
+let mailOptions = {
+    from: 'agweraenock76@gmail.com', 
+    to: 'agwenchezpoetry@gmail.com',
+    subject: 'Agwenchez kwenye programming',
+    text: 'this thing has to work, hatutaki mchezo!!'
+};
+
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        return log('Error occurs');
+    }
+    return log('Email sent!!!');
+});
